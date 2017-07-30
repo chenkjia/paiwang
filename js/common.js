fd3adbbe5d4fb84ebc1e25f5d7cc84a9b083c7c9
frank <798356548@qq.com>
2017-07-30 23:05:32 +0800

    修改头部跟脚步引入方式

--- js/common.js
+++ js/common.js
@@ -5,3 +5,33 @@ function linkHtml (link, top) {
     window.location.href=link
   }
 }
+
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'header.html',
+    dataType: 'html',
+    async : false,
+    success: (data) => {
+      $('#header').html(data)
+    }
+  })
+  $.ajax({
+    type: 'GET',
+    url: 'footer.html',
+    dataType: 'html',
+    async : false,
+    success: (data) => {
+      $('#footer').html(data)
+    }
+  })
+  $.ajax({
+    type: 'GET',
+    url: 'headerBottom.html',
+    dataType: 'html',
+    async : false,
+    success: (data) => {
+      $('#header-bottom').html(data)
+    }
+  })
+})
