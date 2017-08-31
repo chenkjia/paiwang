cd7714e19be109c2c8ecd8b720921c07b37a3bcb
chenkjia <chenkjia@live.com>
2017-08-31 21:58:14 +0800

    小调整

--- js/common.js
+++ js/common.js
@@ -69,3 +69,21 @@ $.ajax({
     $('body').append($(d));
   }
 });
+
+$(document).ready(function(){
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+  $('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+  $('#back-to-top').tooltip('show');
+});
